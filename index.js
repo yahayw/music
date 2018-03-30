f262c433f680976855e235df3f4a3dc2f01f754f
lynn <1768243657@qq.com>
2018-03-30 16:05:26 +0800

    addfile

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ $(function(){
 	$(".nav-list>.hot").on("click",function(){
 		//Promise的方法.then()来做一件事：
 		//成功获取page2.json文件就将page2.json文件内容弄成“热歌榜的页面内容”					
+		$(".loadingCt>img").addClass("loading");	
 		let $ulCt = $('.hotSgLt>.song-list');
 		if($ulCt.attr("data-songLoaded")!=="yes"){
 			$.get("./page2.json").then(function(response){
-				$(".loadingCt>img").addClass("loading");	
 				let {bgUrl,bgSgs} = response;
 				let items = bgSgs;
 				//生成热歌榜封面
@@ -42,13 +42,13 @@ $(function(){
 				$img.attr("src",bgUrl);
 				$img.appendTo($hotCover);
 				$(".pageContent .hotSgLt").prepend($hotCover);
-				initSongList(items,$ulCt,20);//生成歌曲列表这页面中的一个部分
-				$(".loadingCt>img").removeClass("loading");						
+				initSongList(items,$ulCt,20);//生成歌曲列表这页面中的一个部分				
 				//生成底部歌曲链接
 				let $checkLink = $("<a href='#' class='checkLink'>查看完整歌单></a>")
 				$ulCt.append($checkLink);
 		    })
 		}		
+		$(".loadingCt>img").removeClass("loading");				
 	    $ulCt.attr("data-songLoaded","yes");
 	})
 	
