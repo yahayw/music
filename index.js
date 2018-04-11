1192a7bda94544ec5a7cb081732715f629c83454
lynn <1768243657@qq.com>
2018-04-11 10:02:41 +0800

    add

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ $(function(){
 		$(".loadingCt>img").removeClass("loading");
 	})
 	$(".pageContent .playlist li").each(function(index){
-		// console.log("index:",index);
 		$(this).find("a").attr("href",`./playlist.html`)
 	})
 
@@ -20,7 +19,7 @@ $(function(){
 		$(".pageContent>li").eq(index).addClass("active").siblings().removeClass("active");
 	})
 
-	//功能：点击“热歌榜”，发送AJAX请求得到json文件，将文件内容实现成html页面
+	//功能：点击“热歌榜”，发送请求得到json文件，将文件内容实现成html页面
 	$(".nav-list>.hot").on("click",function(){				
 		$(".loadingCt>img").addClass("loading");	
 		let $ulCt = $('.hotSgLt>.song-list');
@@ -81,5 +80,61 @@ $(function(){
 			$ulCt.append($li);
 		})		
 	}
+    let $output = $(".search .srch-result .song-list");
+    
+    //实现搜索歌曲功能
+    $("#srch-song").on('input',function(){    	
+    	$(".search-area .clear").removeClass("hide");
+    	$(".search .hot-search").addClass("hide");
+    	let keyword = $(this).val().trim();
+    	if(keyword===""){return;}
+    	$output.removeClass("hide");    	 
+    	if(timer){
+    		clearTimeout(timer)
+    	}
+	    timer = setTimeout(function(){
+			search(keyword).then((response)=>{
+				clearTimeout(timer);
+				if(response.length!==0){
+					console.log(1);
+					$output.text("");
+					let length = response.length;
+    				initSongList(response,$output,length);
+				}
+				else {
+					console.log(2);
+					$output.text("我们网易云没有这首歌");
+				}		    			
+    		});	    		
+    	},1000);   	
+    })
+    $("#srch-song").on("focus",function(){
+    	let con = $(this).val();    	
+    	if(con===""){
+	    	$(".search-area .clear").removeClass("hide");
+	    	$(".search .hot-search").addClass("hide");
+	    }
+    })
+    function search(keyword){
+    	return new Promise((resolve,reject)=>{
+    		let database = [{ "id":1,"name":"夜空中最亮的星", "singer":"逃跑计划"},{"id":2,"name":"她说","singer":"林俊杰"},{"id":3,"name":"曹操","singer":"林俊杰"},{"id":4,"name":"当你","singer":"林俊杰"},{"id":5,"name":"醉赤壁","singer":"林俊杰"},{"id":6,"name":"江南","singer":"林俊杰"},{"id":7,"name":"年轮","singer":"张碧晨"},{"id":8,"name":"越长大越孤单","singer":"牛奶咖啡"},{"id":9,"name":"扇子舞","singer":"李常超"},{"id":10,"name":"牵丝戏","singer":"银临"},{"id":11,"name":"锦鲤抄","singer":"银临"},{"id":12,"name":"老街","singer":"李荣浩"},{"id":13,"name":"超级英雄","singer":"邓超"},{"id":14,"name":"无敌","singer":"邓超"},{"id":15,"name":"倩女幽魂","singer":"张国荣"},{"id":16,"name":"我们不该这样的","singer":"张赫宣"},{"id":17,"name":"还魂门","singer":"胡彦斌"},{"id":18,"name":"男人KTV","singer":"胡彦斌"}, {"id":19,"name":"喜欢你","singer":"邓紫棋"},{"id":20,"name":"再见","singer":"邓紫棋"}];
+	    	let result = database.filter((item)=>{ return item.name.indexOf(keyword)>=0 || item.singer.indexOf(keyword)>=0});
+	    	resolve(result);
+    	})
+    }
 
+    //清空歌曲输入框
+    $(".search-area .clear").on("click",function(){
+    	$("#srch-song").val("");
+    	$(this).addClass("hide");
+    	$output.addClass("hide");
+    	$(".search .hot-search").removeClass("hide");
+    })
+
+    //点击热门搜索，显示搜索结果
+    $(".search .hot-search button").on("click",function(){
+    	let $this = $(this);
+    	let keyword = $this.text();
+    	search(keyword);
+    })
 })
